58983bb4aec0daabf57ea9a5f671de065c76a567
zNiGhTFuRyZNTT <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2021-04-16 16:20:24 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,54 +1,23 @@
-import socket
-import sys
+from simple_websocket_server import WebSocketServer, WebSocket
 
-def send_commands(s, conn):
-    """Get a command from the user and send it to the client."""
-    print("\nCtrl + C to kill the connection.\n")
-    # test 
-    data1 = conn.recv(4096).decode()
+
+class Connect(WebSocket):
+    def handle(self):
+        print(f"{self.data}", end="")
+        cmd = input()
+        if len(cmd) > 0:
+            self.send_message(cmd)
+            print(self.data)
+
+
+    def connected(self):
+        print(self.address, 'connected')
+
     
-    # ------
-    print(f"{data1}", end="")
-    while True:
-        try:
-            cmd = input()
-            if len(cmd) > 0:
-                conn.sendall(cmd.encode())
-                data = conn.recv(4096) # avaz nashe, high capacity
-                print(data.decode("utf-8"), end="")
-        except KeyboardInterrupt:
-            print("\nGoodbye.")
-            conn.close()
-            sys.exit()
-        except Exception as e:
-            print(e)
-            conn.close()
-            e.close()
-            sys.exit()
 
-def server(address):
-    """Initialize a socket server and wait for connections."""
-    try:
-        s = socket.socket()
-        s.bind(address)
-        s.listen()
-        print("Server Initialized. I'm listening...")
-    except Exception as e:
-        print("\nIt seems like something went wrong.")
-        print(e)
-        restart = input("\nDo you want me to reinitialize the server? y/n ")
-        if restart.lower() == "y" or restart.lower() == "yes":
-            print("\nRoger That. Reinitializing the server...\n")
-            server(address)
-        else:
-            print("\nSo Long, and Thanks for All the Fish.\n")
-            sys.exit()
-    conn, client_addr = s.accept()
-    print(f"Connection Established: {client_addr}")
-    send_commands(s, conn)
+    
+    def handle_close(self):
+        print(self.address, 'closed')
 
-if __name__ == "__main__":
-    host = "127.0.0.1"
-    port = 5050
-    server((host, port))
-        
+server = WebSocketServer('127.0.0.1', 5050, Connect)
+server.serve_forever()
